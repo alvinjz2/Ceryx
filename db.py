ac3d79ba75fa5d90c4ef8ab9adf0510c0b9905a8
Alvin Zhang <alvin.zhang295@gmail.com>
2022-07-02 22:52:24 -0700

    Update db.py

--- db.py
+++ db.py
@@ -4,14 +4,20 @@ class database:
     def __init__(self, db_name) -> None:
         self.db = None
         try:
-            self.db = sqlite3.connect(db_name)
+            self.db = sqlite3.connect(f'{db_name}.db')
             self.c = self.db.cursor()
         except:
             print(f'Could not connect to database')
             raise Exception
+        self.c.execute("""Create Table If Not Exists Permissions (username text, password text, permissions text) """)
 
-    def select(self) -> None:
+    def select(self, username, password) -> None:
+        t = self.c.execute("""Select Permissions from Permissions Where username=? and password=?""", (username, password))
+        for row in t:
+            print(row)
         return None
+    def insert(self) -> None:
+        t = self.c.execute("""Insert into Permissions (username, password, permissions) Values ('quant', 'quant', 'all')""")
 
     def delete(self) -> None:
         return None
@@ -22,3 +28,7 @@ class database:
         except:
             print(f'Could not close connection')
             raise Exception
+
+d = database('permissions')
+d.insert()
+d.select('quant', 'quant')
