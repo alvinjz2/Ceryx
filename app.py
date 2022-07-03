a107883adda26ed545d4f8a173b0f48108753583
Alvin Zhang <alvin.zhang295@gmail.com>
2022-07-03 16:59:33 -0700

    Restructure code

--- app.py
+++ app.py
@@ -1,57 +1,29 @@
 from flask import Flask, request, redirect, abort
-import threading
-import asyncio
-import websockets
+from flask import render_template
+
 app = Flask(__name__)
 app.config.from_object('config')
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
 
-def valid_request(request):
-    accepted_brokers = ['IB'] 
-    accepted_orders = []
-    accepted_order_types = []
-    valid_parameters = ['api_key', 'broker', 'timestamp', 'order', 'order_type', 'instrument', 'quantity', 'spot']
-    error_code = ''
-    if request[valid_parameters[0]] != 'alvinjz2':
-        return (False, '0')
-    if request[valid_parameters[1]] not in accepted_brokers:
-        error_code += '1'
-    if int(request[valid_parameters[6]]) <= 0:
-        error_code += '6'
-    return (False, error_code) if error_code != '' else (True, error_code)
-
-@app.route('/not_authorized')
-def deny():
-    return 'Not authorized.'
-
-@app.route("/buy", methods=['GET','POST'])
+@app.route("/buy", methods=['POST'])
 def execute_buy():
-    if request.method == 'GET':
-        token = request.args.get('api_key')
-        if token == None:
-            return "No API Key"
-        elif token != 'alvinjz2':
-            return redirect('/not_authorized')
-        return "Success"
-    else:
-        return "Default"
+    return None
 
 
-@app.route("/sell", methods=['GET','POST'])
+@app.route("/sell", methods=['POST'])
 def execute_sell():
-    if request.method == 'GET':
-        token = request.args.get('api_key')
-        if token == None:
-            return "No API Key"
-        elif token != 'alvinjz2':
-            return redirect('/not_authorized')
-        return "Success"
-    else:
-        return "Default"
+    return None
 
-@app.route('/json-example', methods=['GET','POST'])
-def json_example():
-    return 'JSON Object Example'
+
+@app.route("/openorders", methods=['GET'])
+def get_openorders():
+    return None
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+if __name__ == '__main__':
+    app.run(threaded=True)
