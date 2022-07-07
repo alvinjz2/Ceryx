340e011eb2eceedcab0cc799edabecc483a55b97
Alvin Zhang <alvin.zhang295@gmail.com>
2022-07-06 22:20:56 -0700

    Redo app.py
    Asynchronous handling

--- app.py
+++ app.py
@@ -1,27 +1,41 @@
-from flask import Flask, request, redirect, abort
+from aioflask import Flask, request, Response
 from flask import render_template
+import asyncio
 
 app = Flask(__name__)
 app.config.from_object('config')
 
 
 @app.route("/buy", methods=['POST'])
-def execute_buy():
+async def execute_buy():
+    allowed = await Authenticate(request.args.get('token'), 'buy')
+    if not allowed: 
+        return 
     raise NotImplemented
 
 
 @app.route("/sell", methods=['POST'])
-def execute_sell():
+async def execute_sell():
     raise NotImplemented
+
 
 @app.route("/openorders", methods=['GET'])
-def get_openorders():
+async def get_openorders():
     raise NotImplemented
 
 
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
+@app.route("/profitandloss", methods=['GET'])
+async def get_profitandloss():
+    raise NotImplemented
+
+
+async def Authenticate(token, method):
+    # Check if token exists in the permissions table.
+    # Client will have a call to see if the user exists (token associated with user)
+    # but permissions for Ceryx are not defined. 
+    raise NotImplemented
+
+
 
 
 if __name__ == '__main__':
