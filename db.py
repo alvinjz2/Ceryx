35871803f7e97f5c04f7060fab0110e5d0ace657
Alvin Zhang <alvin.zhang295@gmail.com>
2022-07-22 15:45:43 -0700

    Update db.py
    Add table scheme and get user info function basics

--- db.py
+++ db.py
@@ -1,30 +1,40 @@
 import sqlite3
+"""
+table scheme:
+token user pass admin read write
+
+token: access token to call API endpoints
+expire time: when it becomes expired
+user: username, hashed
+pass: password, hashed
+admin: can generate new user sign up keys, can modify other read write permissions
+read: only get requests
+write: only post requests
+
+token to api request because what if temp user, might not need to store info in db
+
+"""
+
 
 class Database:
     def __init__(self, db_name) -> None:
         self.db = None
         try:
-            self.db = sqlite3.connect(f'{db_name}.db')
+            self.db = sqlite3.connect(f'Quant.db')
             self.c = self.db.cursor()
         except:
-            print(f'Could not connect to database')
             raise Exception
         self.c.execute("""Create Table If Not Exists ? (username text, password text, permissions text) """, (db_name))
 
     def is_user(self, username, password) -> None:
-        t = self.c.execute("""Select Token from Permissions Where username=? and password=?""", (username, password))
+        t = self.c.execute("""Select * from Permissions Where username=? and password=?""", (username, password))
         for row in t:
             print(row)
         return None
 
-    def get_token(self, username, password) -> None:
+    def get_userinfo(self, username, password) -> None:
         raise NotImplemented
-    def insert(self) -> None:
-        t = self.c.execute("""Insert into Permissions (username, password, permissions) Values ('quant', 'quant', 'all')""")
 
-    def delete(self) -> None:
-        return None
-        
     def __del__(self) -> None:
         try:
             self.c.close()
