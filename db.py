8cd36deaccaf3a8c0a42a0d9ed388e0817622d2a
Alvin Zhang <alvin.zhang295@gmail.com>
2022-07-24 21:37:54 -0700

    Update db.py
    create log table, select helper function, insert helper function, and add user function

--- db.py
+++ db.py
@@ -20,20 +20,40 @@ class Database:
     def __init__(self, db_name) -> None:
         self.db = None
         try:
-            self.db = sqlite3.connect(f'Quant.db')
+            self.db = sqlite3.connect(f'{db_name}.db')
             self.c = self.db.cursor()
         except:
             raise Exception
-        self.c.execute("""Create Table If Not Exists ? (username text, password text, permissions text) """, (db_name))
+        self.c.execute("""Create Table If Not Exists Users 
+        (token text default null, expire_time text default null, 
+        username text default null, password text default null, 
+        admin integer default 0, read integer default 1, write integer default 0) """)
 
-    def is_user(self, username, password) -> None:
-        t = self.c.execute("""Select * from Permissions Where username=? and password=?""", (username, password))
-        for row in t:
-            print(row)
-        return None
+        self.c.execute("""Create Table If Not Exists Log 
+        (timestamp text default null, token text default null, method text default null) """)
 
-    def get_userinfo(self, username, password) -> None:
-        raise NotImplemented
+    def add_action(self):
+        params = ('Log')
+
+    def add_user(self, token, username="", password="", expire = "", admin = "", read = 1, write = 0):
+        params = ('Users', ('token', 'expire', 'username', 'password', 'admin', 'read', 'write'), (token, expire, username, password, admin, read, write))
+        return self.insert_row(params)
+
+    def insert_row(self, params):
+        # Table, Columns, Data
+        try:
+            self.c.execute("""Insert into ? ? Values ? """, params)
+            return True
+        except:
+            return False
+        
+    def get_userinfo(self, username, password, token=""):
+        params = ('Users', username, password, token)
+        return self.select_row(params)
+
+    def select_row(self, params):
+        resp = self.c.execute("""Select * from ? Where (username=? AND password=?) OR token = ?""", params)
+        return resp
 
     def __del__(self) -> None:
         try:
