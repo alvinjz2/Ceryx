06c839e04745752dd05e231fc3f5f1f5b9f2ede7
Alvin Zhang <alvin.zhang295@gmail.com>
2022-08-01 18:22:28 -0700

    Add logging
    Implement LogAction function which writes tracks actions performed

--- app.py
+++ app.py
@@ -27,31 +27,45 @@ def gone(error):
 @app.route("/buy", methods=['POST'])
 async def execute_buy():
     token = request.args.get('token')
-    if await AllowedWrite(token):
-        return True
+    resp = await AllowedWrite(token)
+    if resp[0]:
+        await LogAction(resp[1], token, 'buy')
+        return "Success"
+    else:
+        return "Failed"
 
 
 @app.route("/sell", methods=['POST'])
 async def execute_sell():
     token = request.args.get('token')
-    if await AllowedWrite(token):
-        return True
+    resp = await AllowedWrite(token)
+    if resp[0]:
+        await LogAction(resp[1], token, 'sell')
+        return "Success"
+    else:
+        return "Failed"
 
 
 @app.route("/openorders", methods=['GET'])
 async def get_openorders():
     token = request.args.get('token')
-    if await AllowedRead(token):
-        return True
-
+    resp = await AllowedRead(token)
+    if resp[0]:
+        await LogAction(resp[1], token, 'getopenorders')
+        return "Success"
+    else:
+        return "Failed"
 
 @app.route("/profitandloss", methods=['GET'])
 async def get_profitandloss():
     token = request.args.get('token')
-    if await AllowedRead(token):
-        return True
-
-
+    resp = await AllowedRead(token)
+    if resp[0]:
+        await LogAction(resp[1], token, 'getprofitandloss')
+        return "Success"
+    else:
+        return "Failed"
+        
 @app.route("/authenticate", methods=['POST'])
 async def Authenticate():
     # Check if token exists in the permissions table.
@@ -60,57 +74,70 @@ async def Authenticate():
     # could have read/write permission sets, aka two tables for each
     username, password = request.args.get('user'), request.args.get('pw')
     resp = await UserInfo(username, password)
-    if time_expired(resp[UserDetail.expired.value]):
+    if time_expired(resp[0][UserDetail.expired.value]):
         abort(410)
-    return resp[UserDetail.token.value]
+    return resp[0][UserDetail.token.value]
 
 @app.route("/register", methods=['POST'])
 async def Register():
     token = request.args.get('token')
-    if await UserInfo(token=token):
-        new_user, new_pw, exp_time = request.args.get('new_user'), request.args.get('new_pw'), request.args.get('exp_time')
-        Quant = Database('Quant')
+    new_user, new_pw, exp_time = request.args.get('new_user'), request.args.get('new_pw'), request.args.get('exp_time')
+    resp = await UserInfo(token=token)
+    if resp[0][UserDetail.admin.value]:
         if new_user == None or new_pw == None:
             if exp_time == None:
                 raise Exception 
-            resp = Quant.add_user(None, None, exp_time)
+            req = resp[1].add_user(None, None, exp_time)
         else:
-            resp = Quant.add_user(new_user, new_pw, exp_time)
-    return resp
+            req = resp[1].add_user(new_user, new_pw, exp_time)
+    return "Success" if req else "Failed"
 
 
 async def UserInfo(username=None, password=None, token=None):
-    Quant = Database('Quant')
+    db = Database('Quant')
     if username != None and password != None:
-        resp = Quant.get_userinfo_via_userpass(username, password)
+        resp = db.get_userinfo_via_userpass(username, password)
     elif token != None:
-        resp = Quant.get_userinfo_via_token(token)
+        resp = db.get_userinfo_via_token(token)
     else:
         # Unsupported auth method
-        return Exception
+        abort(401)
+    print(resp)
     if resp is None:
         abort(401)
-    return resp
+    return resp, db
     
 async def AllowedWrite(token):
     resp = await UserInfo(token=token)
-    if not resp:
-        return False
-    if time_expired(resp[UserDetail.expired.value]):
+    if not resp[0]:
+        return False, resp[1]
+    if time_expired(resp[0][UserDetail.expired.value]):
         abort(410)
-    if resp[UserDetail.read.value]:
-        return True
+    if resp[0][UserDetail.read.value]:
+        return True, resp[1]
+    else:
+        abort(403)
 
 async def AllowedRead(token):
     resp = await UserInfo(token=token)
-    if not resp:
-        return False
-    if time_expired(resp[UserDetail.expired.value]):
+    if not resp[0]:
+        return False, resp[1]
+    if time_expired(resp[0][UserDetail.expired.value]):
         abort(410)
-    if resp[UserDetail.read.value]:
-        return True
+    if resp[0][UserDetail.read.value]:
+        return True, resp[1]
     else:
         abort(403)
+
+async def LogAction(db, token, action):
+    try:
+        current_time = datetime.datetime.now()
+        date = datetime.datetime.strftime(current_time, "%m/%d/%Y")
+        timestamp = datetime.datetime.strftime(current_time, "%H:%M:%S:%f")
+        db.add_action(date, timestamp, token, action)
+    except:
+        abort(403)
+
 
 def time_expired(expire_date):
     return True if datetime.datetime.now() > datetime.datetime.strptime(expire_date, '%m/%d/%Y') else False
